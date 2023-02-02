195e8d585dc53bfdfcd41b258d1b92b72b9b7c49
SkylerVue <97487457+SkylerVue@users.noreply.github.com>
2023-02-02 11:12:24 -0600

    fixed StockMarket

--- StockMarket.java
+++ StockMarket.java
@@ -3,18 +3,23 @@ public class StockMarket extends Subject {
     @Override
     public void notifyObservers() {
         // TODO Auto-generated method stub
-        
+        StockData genStockData = new StockData();
+        for (Observer obs : observers()) {
+            genStockData.stockGenerator();
+            obs.update(genStockData);
+        }
+
     }
 
-    public void run(){
-        while(true){
+    public void run() {
+        while (true) {
             notifyObservers();
-            try{
+            try {
                 Thread.sleep(1000);
-            } catch (InterruptedException e){
+            } catch (InterruptedException e) {
             }
-            
+
         }
     }
-    
+
 }
