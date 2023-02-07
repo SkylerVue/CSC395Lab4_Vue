b135c33065d82b55fa2420de8a0215893ca4edd7
SkylerVue <97487457+SkylerVue@users.noreply.github.com>
2023-02-07 11:05:59 -0600

    updated StockWatcher

--- StockWatcher.java
+++ StockWatcher.java
@@ -1,3 +1,16 @@
-public class StockWatcher {
-    
+public class StockWatcher implements Observer{
+
+    public StockWatcher(StockMarket stockMarket) {
+    }
+
+    public void updateStocks(StockData data){
+
+    }
+
+    @Override
+    public void update(Object newData) {
+        if(newData instanceof StockData){
+            updateStocks((StockData) newData);
+        }   
+    }
 }
