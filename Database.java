3b1e89b92b1ecc165e8402cc12fa4e09c4ec60fb
Dori <denmand@augsburg.edu>
2023-02-07 11:05:38 -0600

    database done

--- Database.java
+++ Database.java
@@ -1,3 +1,19 @@
-public class Database {
-    
+public class Database implements Observer {
+    StockData stocks;
+    StockMarket stockMarket;
+
+    public Database(StockMarket data){
+       
+    }
+
+    private void updateStocks(StockData data) {
+    }
+
+    public void update(Object newData) {
+        if (newData instanceof StockData) {
+            updateStocks((StockData) newData);
+        }
+        
+    }
+
 }
--- NewsAP.java
+++ NewsAP.java
@@ -1,3 +1,24 @@
-public class NewsAP {
-    
+public class NewsAP extends Subject {
+    NewsData NewsData;
+
+    public void notifyObservers() {
+        for (Observer obs : observers()) {
+            NewsData nData = new NewsData();
+            nData.headlineGenerator();
+            obs.update(nData);
+        }
+
+    }
+
+    public void run() {
+        while (true) {
+            notifyObservers();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+
 }
